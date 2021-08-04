ae28171b34d14bf3b2190c993e0703ade98c0c76
vanarok <ivanisekeev0@gmail.com>
2021-08-04 13:08:46 +0400

    delete fs path

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const got = require('got');
 const port = 6745
 const basePath = '/api'
 const companyId = '494680'
-const authTokenUser = "7ab7473da117707ff4babe079e3dcb09"
-const authTokenBearer = '80faa2893eb9499e444f0c1ada89bfb9'
+const authTokenUser = "80faa2893eb9499e444f0c1ada89bfb9"
+const authTokenBearer = 'kdbsg2ujeseu99a3uk7e'
 const authToken = 'Bearer ' + authTokenBearer + ', User ' + authTokenUser
 const staffIdList = {
     "casinorobbery": ['1528160', '1528163'],
